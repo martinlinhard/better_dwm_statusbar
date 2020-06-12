54c6a7a14447cc299390ff2236af7f5d1ca15fc9
Martin Linhard <linmad17@htl-kaindorf.at>
2020-06-12 08:30:47 +0200

    Fixed customscript implementation

--- src/bar_modules/custom_script.rs
+++ src/bar_modules/custom_script.rs
@@ -1,28 +1,44 @@
 use crate::module::Module;
-use std::process::Command;
+use std::error::Error;
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
 
 pub struct CustomScript {
     pub format: &'static str,
-    pub command: Command,
+    /// This has to be the content of a script! (or just a normal command)
+    pub command: &'static str,
 }
 
 impl CustomScript {
-    pub fn new(format: &'static str, command: Command) -> Self {
+    pub fn new(format: &'static str, command: &'static str) -> Self {
         Self { format, command }
     }
 }
 
 impl Module for CustomScript {
     fn yield_next_value(&mut self) -> String {
-        // If the output is okay, we want to convert it to a string
-        let output = self
-            .command
-            .output()
-            .map(|output| String::from_utf8(output.stdout).unwrap());
-
-        // If the output was an error, we want to convert it to an empty string
-        let content = output.unwrap_or(String::new());
-
+        let content = execute_in_bash(self.command).unwrap_or(String::new());
         self.format.replace(crate::PLACEHOLDER, content.trim())
     }
+}
+
+pub fn execute_in_bash(value: &str) -> Result<String, Box<dyn Error>> {
+    let process = Command::new("bash")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    process
+        .stdin
+        .ok_or(std::io::Error::new(std::io::ErrorKind::Other, "Fail"))?
+        .write_all(value.as_bytes())?;
+
+    let mut s = String::new();
+
+    process
+        .stdout
+        .ok_or(std::io::Error::new(std::io::ErrorKind::Other, "Fail"))?
+        .read_to_string(&mut s)?;
+
+    Ok(s)
 }
